13d28b88e21f5df8789397c1be069d6a0b211464
Elena <56148816+elenajusto@users.noreply.github.com>
2024-08-20 16:43:41 +1000

    wrote basic unit test to see if barometer and SD card is connected. removed LED and buzzer from unit testing

--- test/test_bmp280/test_main.cpp
+++ test/test_bmp280/test_main.cpp
@@ -1,9 +1,14 @@
 #include <Arduino.h>
 #include <unity.h>
 
+void test_barometer() {
+  Adafruit_BMP280 barometer = Adafruit_BMP280(&Wire);       // Initialise barometer library object
+  TEST_ASSERT_TRUE(barometer.begin());                      // Connect to barometer
+}
 
 void setup() {
   UNITY_BEGIN();
+  RUN_TEST(test_barometer);
   UNITY_END();
 }
 
--- test/test_sd/test_main.cpp
+++ test/test_sd/test_main.cpp
@@ -1,9 +1,13 @@
 #include <Arduino.h>
 #include <unity.h>
 
+void test_sd() {
+  TEST_ASSERT_TRUE(SD.begin(4));    // Connect to SD card
+}
 
 void setup() {
   UNITY_BEGIN();
+  RUN_TEST(test_sd);
   UNITY_END();
 }
 
