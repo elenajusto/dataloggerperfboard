d926254231b01d6e78e2a9e43634ca7bf7b1c233
Elena <56148816+elenajusto@users.noreply.github.com>
2024-08-29 11:17:59 +1000

    modified to atmega328p environment. sensors still failing to init

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,19 @@
 #include <Arduino.h>
+#include <Adafruit_BMP280.h>
 
-// put function declarations here:
-int myFunction(int, int);
+Adafruit_BMP280 bmp; // I2C
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  Serial.begin(9600);
+  Serial.println("Starting BMP");
+  Serial.println(bmp.begin());
+  delay(5000);
+  bmp.begin();
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-}
-
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+  Serial.print(F("Approx altitude = "));
+  Serial.print(bmp.readAltitude(1013.25)); /* Adjusted to local forecast! */
+  Serial.println(" m");
 }
